3f8677b15e0910be5577435c565a36348fb801aa
StoneySpring688 <66467256+StoneySpring688@users.noreply.github.com>
2025-11-05 16:10:16 +0100

    Add files via upload

---
+++ encuestas/src/main/java/encuestas/modelo/Encuesta.java
@@ -0,0 +1,114 @@
+package encuestas.modelo;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import repositorio.Identificable;
+import utils.LocalDateTimeAdapter;
+
+
+@XmlRootElement
+@Entity
+public class Encuesta implements Identificable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+	
+	private String titulo;
+	@Lob //tipo mas largo por eso ponemos esto pq sino VARCHAR255
+	private String instrucciones;
+	
+	private LocalDateTime apertura;
+	
+	private LocalDateTime cierre;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "encueta_fk")
+	private LinkedList<Opcion> opciones = new LinkedList<>();
+	
+	public Encuesta() { // POJO
+		
+	}
+	
+	public Encuesta(String titulo, String instrucciones, LocalDateTime apertura, LocalDateTime cierre,
+			List<String> opcionesTexto) {
+		
+		this.titulo = titulo;
+		this.instrucciones = instrucciones;
+		this.apertura = apertura;
+		this.cierre = cierre;
+		this.opciones = new LinkedList<>();
+		
+		for (String opcionTexto : opcionesTexto)
+			this.opciones.add(new Opcion(opcionTexto));
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getInstrucciones() {
+		return instrucciones;
+	}
+	public void setInstrucciones(String instrucciones) {
+		this.instrucciones = instrucciones;
+	}
+	
+	@XmlJavaTypeAdapter(value=LocalDateTimeAdapter.class)
+	public LocalDateTime getApertura() {
+		return apertura;
+	}
+	public void setApertura(LocalDateTime apertura) {
+		this.apertura = apertura;
+	}
+	
+	@XmlJavaTypeAdapter(value=LocalDateTimeAdapter.class)
+	public LocalDateTime getCierre() {
+		return cierre;
+	}
+	public void setCierre(LocalDateTime cierre) {
+		this.cierre = cierre;
+	}
+	public LinkedList<Opcion> getOpciones() {
+		return opciones;
+	}
+	public void setOpciones(LinkedList<Opcion> opciones) {
+		this.opciones = opciones;
+	}
+	
+	// Propiedad calculada
+	
+	public int getNumeroOpciones() {
+		
+		return this.opciones.size();
+	}
+
+	@Override
+	public String toString() {
+		return "Encuesta [id=" + id + ", titulo=" + titulo + ", instruccion=" + instrucciones + ", apertura=" + apertura
+				+ ", cierre=" + cierre + ", opciones=" + opciones + "]";
+	}
+	
+}
---
+++ encuestas/src/main/java/encuestas/modelo/Opcion.java
@@ -0,0 +1,63 @@
+package encuestas.modelo;
+
+import java.util.LinkedList;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Opcion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private String texto;
+	
+	@ElementCollection
+	private LinkedList<String> votos = new LinkedList<>();
+	
+	public Opcion() { // POJO
+		
+	}
+	
+	public Opcion(String texto) {
+		this.texto = texto;
+	}
+	
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LinkedList<String> getVotos() {
+		return votos;
+	}
+	public void setVotos(LinkedList<String> votos) {
+		this.votos = votos;
+	}
+	
+	// Propiedad calculada
+	
+	public int getNumeroVotos() {		
+		return this.votos.size();
+	}
+	
+	@Override
+	public String toString() {
+		return "Opcion [texto=" + texto + ", votos=" + votos + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+}
