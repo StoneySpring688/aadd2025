077c34c7e664b42598a6b6e95ac633b2719c329e
alberto.z.m <alberto.z.m@um.es>
2025-11-08 12:17:31 +0100

    feat: repositorio categoriasXML

---
+++ SegundUM/src/main/java/stoneyspring/SegundUM/servicio/categorias/RepositorioCategoriasXML.java
@@ -0,0 +1,13 @@
+package stoneyspring.SegundUM.servicio.categorias;
+
+import stoneyspring.SegundUM.dominio.Categoria;
+import stoneyspring.SegundUM.repositorio.RepositorioXML;
+
+public class RepositorioCategoriasXML extends RepositorioXML<Categoria> {
+
+	@Override
+	public Class<Categoria> getClase() {
+		return Categoria.class;
+	}
+
+}
